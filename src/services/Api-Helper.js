e145c940f67b537cb544d8b1d71dcc9fedad68e1
Nazrul Babu <n95babu@gmail.com>
2019-10-21 15:54:08 -0400

    Added MapBox

--- src/services/Api-Helper.js
+++ src/services/Api-Helper.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const APIKEY = `FYD6WnexnDMX3fTG8s94IU98t`;
-const BaseUrl = `https://data.cityofnewyork.us/resource/hjae-yuav.json?%24limit=5000&%24%24app_token=${APIKEY}`;
+const BaseUrl = `https://data.cityofnewyork.us/resource/hjae-yuav.json?%24limit=5000&%24%24app_token=${process.env.REACT_APP_CITI_ACCESS_TOKEN}`;
 
 export const fetchBathrooms = async () => {
   const response = await axios.get(`${BaseUrl}`);
